b63dfe37c25407a7d04e9e3066baa7b5f0e4bea4
luanth <luanth@gmail.com>
2019-11-13 17:21:19 +0700

    build to bum.github.io

--- theme-embedable/vue.config.js
+++ theme-embedable/vue.config.js
@@ -1,4 +1,5 @@
 const PUB_PATH = 'https://bum.github.io/bootstrap-color/theme-embedable/'
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? PUB_PATH : '/',
+  outputDir: 'D:/WIN7/Dropbox/ws/nodews/bum.github.io/bootstrap-color/theme-embedable',
 }
